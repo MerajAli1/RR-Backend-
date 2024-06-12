8a9a6c6647d2b1dd859f8244fc69fcb67b5f9f61
MerajAli1 <merajali45425@gmail.com>
2024-06-12 13:30:13 -0700

    Some Changes

--- src/app.js
+++ src/app.js
@@ -3,13 +3,9 @@ import cors from "cors"
 import express, { urlencoded } from "express"
 const app = express()
 
-app.use(cors({
-    origin:["https://rr-backend-two.vercel.app/","http://localhost:8003"],
-    methods:["GET","POST","PUT","DELETE"],
-    credentials:true
-}))
-app.use(express.json({limit:"16kb"}))
-app.use(urlencoded({extended:true,limit: "16kb"}))
+app.use(cors())
+app.use(express.json({ limit: "16kb" }))
+app.use(urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 
 // app.use(CookieParser())
@@ -18,10 +14,10 @@ import ProductRouter from "./routes/Product.routes.js"
 import orderRouter from "./routes/Order.routes.js"
 import contactRouter from "./routes/Contact.routes.js"
 import curoselRouter from "./routes/Curosel.route.js"
-app.use("/api/v1",ProductRouter)
-app.use("/api/v1",orderRouter)
-app.use("/api/v1",contactRouter)
-app.use("/api/v1",curoselRouter)
+app.use("/api/v1", ProductRouter)
+app.use("/api/v1", orderRouter)
+app.use("/api/v1", contactRouter)
+app.use("/api/v1", curoselRouter)
 // http://localhost:8000/api/v1/Products
 
-export {app}
+export { app }
