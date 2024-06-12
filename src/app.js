a11df44923e44686b7399cc0909ea5a49add1983
MerajAli1 <merajali45425@gmail.com>
2024-06-12 10:35:00 -0700

    Added origin in cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ import cors from "cors"
 import express, { urlencoded } from "express"
 const app = express()
 
-app.use(cors())
+app.use(cors({
+    origin:["https://rr-backend-two.vercel.app/","http://localhost:8003"],
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true
+}))
 app.use(express.json({limit:"16kb"}))
 app.use(urlencoded({extended:true,limit: "16kb"}))
 app.use(express.static("public"))
